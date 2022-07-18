cb1572dc83465e04b4d6a6652e9723636977ec99
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2022-07-18 23:59:23 +0600

    user result section in search page

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,40 +1,94 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { Link, useParams } from "react-router-dom";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  startAt,
+  endAt,
+} from "firebase/firestore";
 import BookBasicDetails from "../BookBasicDetails/BookBasicDetails";
 import { db } from "../../firebase";
 
 const SearchPage = () => {
-  const [results, setResults] = useState();
+  const [bookResults, setBookResults] = useState();
+  const [userResults, setUserResults] = useState();
   let { searchQuery } = useParams();
   useEffect(() => {
-    const fetchResults = async () => {
+    const fetchUserResults = async () => {
+      let resultsArray = [];
+      const userDocs = await getDocs(
+        query(
+          collection(db, "users"),
+          orderBy("userName"),
+          startAt(searchQuery),
+          endAt(searchQuery + "\uf8ff")
+        )
+      );
+      console.log(userDocs);
+      userDocs.forEach((userDoc) => {
+        resultsArray.push(userDoc.data());
+      });
+      setUserResults(resultsArray);
+    };
+    const fetchBookResults = async () => {
       let resultsArray = [];
       const queryDocs = await getDocs(
-        query(collection(db, "books"), where("nameIndices", "array-contains", searchQuery))
+        query(
+          collection(db, "books"),
+          where("nameIndices", "array-contains-any", searchQuery.split(" ", 10))
+        )
       );
       queryDocs.forEach((doc) => {
-        resultsArray.push(doc.data());
+        resultsArray.push({ ...doc.data(), id: doc.id });
       });
-      setResults(resultsArray);
+      setBookResults(resultsArray);
     };
-    fetchResults();
+    fetchUserResults();
+    fetchBookResults();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <main className="search-page navbar-included">
       <h1>Results for "{searchQuery}"</h1>
-      
-      <div className="book-results">
-        <h2>Books</h2>
-        <div className="book-results-container">
-          {results ? (
-            results.map((res) => <BookBasicDetails data={res} />)
-          ) : (
-            <>loading...</>
-          )}
-        </div>
-      </div>
+      {bookResults && userResults ? (
+        <>
+          <section className="book-results">
+            <h2>Books</h2>
+            <div className="results-container">
+              {bookResults.length > 0 ? (
+                bookResults.map((res) => (
+                  <Link to={`/${res.id}`}>
+                    <BookBasicDetails data={res} />
+                  </Link>
+                ))
+              ) : (
+                <p>No such book Exists</p>
+              )}
+            </div>
+          </section>
+          <section className="user-results">
+            <h2>Users</h2>
+            <div className="results-container">
+              {userResults.length > 0 ? (
+                userResults.map((res) => (
+                  <Link to={`/userID=${res.uid}`}>
+                    <div className="user-res">
+                      <img src={res.avatarURL} alt="" /> <h3>{res.userName}</h3>
+                    </div>
+                  </Link>
+                ))
+              ) : (
+                <p>No such User Exists</p>
+              )}
+            </div>
+          </section>
+        </>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </main>
   );
 };
